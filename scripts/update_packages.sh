f5743a33a0682a5678a9e7e174255888060c5c0e
Jaime C Marques <jaime.mqu@gmail.com>
2018-08-16 15:58:41 -0300

    Update script test

--- scripts/update_packages.sh
+++ scripts/update_packages.sh
@@ -4,9 +4,21 @@
 rm -rf packages
 
 # update-submodules
-git subtree add --prefix .temp https://github.com/stone-payments/pos-mamba-sdk.git develop --squash
-mkdir packages
-cd .temp/packages/components
-mv * ../../packages
-cd ../.. && rm -rf .temp
+#git subtree add --prefix .temp https://github.com/stone-payments/pos-mamba-sdk.git develop --squash
+
+# mkdir packages
+
+# cd .temp/packages/components
+
+# mv * ../../packages
+
+# cd ../.. && rm -rf .temp
+
+git submodule add --force https://github.com/stone-payments/pos-mamba-sdk.git packages
+
+cd packages
+
+git remote rm origin
+
+git filter-branch --subdirectory-filter packages/components -- --all
 

a0e726be1ba18af476987e227e2827e01aa1c57f
Jaime C Marques <jaime.mqu@gmail.com>
2018-08-16 15:28:06 -0300

    Squashed 'packages/' content from commit a113424
    git-subtree-dir: packages
    git-subtree-split: a113424a3201815236d79d43599c4ced6767f4a4

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const { fromWorkspace, fromProject } = require('./tools/utils/paths.js');
+
+module.exports = {
+  rootDir: fromWorkspace(),
+  collectCoverageFrom: [fromWorkspace('**/*.test.js')],
+  testMatch: [fromWorkspace('**/*.test.js')],
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleDirectories: [
+    fromProject('node_modules'),
+    fromWorkspace('node_modules'),
+    fromWorkspace('src'),
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': fromProject(
+      'tools/jest/__mocks__/fileMock.js',
+    ),
+    '\\.(s[ac]?|c)ss$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.jsx?$': fromProject('tools/jest/babelPreprocess.js'),
+  },
+};

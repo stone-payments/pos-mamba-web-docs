119eb5f1cf8745aa8ab97753e649f18ccaa8b1fd
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-09-10 13:58:48 -0300

    Fix now.sh endpoint.

--- scripts/slack_webhook.js
+++ scripts/slack_webhook.js
@@ -3,14 +3,14 @@ let Slack = require('slack-node');
 if(process.env.NOW) {
 
   webhookUri = "https://hooks.slack.com/services/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";
-   
+
   slack = new Slack();
   slack.setWebhook(webhookUri);
-   
+
   slack.webhook({
     channel: "#pos_mambaweb_squad",
     username: "MAMBA SDK DOCS",
-    text: `Documentação deployada em staging: https://mambasdkdocs.now.sh/ \n Deploy hash: ${process.env.NOW_URL}`,
+    text: `Documentação deployada em staging: https://mambadocs.now.sh/ \n Deploy hash: ${process.env.NOW_URL}`,
   }, function(err, response) {
     console.log(response);
   });

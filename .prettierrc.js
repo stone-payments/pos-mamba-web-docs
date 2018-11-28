89f47c116f8627ed531f80753742e08ce3fd493f
Jaime C Marques <jaime.mqu@gmail.com>
2018-11-28 16:06:27 -0200

    Update prettier with new props

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
+  printWidth: 80,
   semi: true,
   printWidth: lineLength,
+  useTabs: false,
+  tabWidth: 2,
   trailingComma: 'all',
   bracketSpacing: true,
   jsxBracketSameLine: false,

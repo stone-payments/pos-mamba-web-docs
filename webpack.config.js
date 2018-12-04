7cbd7e9b94cab00c0e763ffdc484a6196f59bfaf
Jaime C Marques <jaime.mqu@gmail.com>
2018-12-04 18:12:25 -0200

    Better file formatting with prettier

--- webpack.config.js
+++ webpack.config.js
@@ -6,33 +6,42 @@ const serverConfig = require('./webpack/base.config.js')('server');
 const pkg = require('./package.json');
 const { IS_DEV } = require('quickenv');
 
-
-const client = merge([clientConfig, {
-  stats: "verbose",
-  entry: webpackConfig.client.entry(),
-  output: Object.assign(webpackConfig.client.output(), {
-    globalObject: 'this',
-  }),
-  target: 'web',
-  plugins: [
-    IS_DEV && new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      'process.browser': true,
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+const client = merge([
+  clientConfig,
+  {
+    stats: 'verbose',
+    entry: webpackConfig.client.entry(),
+    output: Object.assign(webpackConfig.client.output(), {
+      globalObject: 'this',
     }),
-  ].filter(Boolean),
-  devtool: IS_DEV && 'inline-source-map',
-}]);
-
-const server = merge([serverConfig, {
-  entry: webpackConfig.server.entry(),
-  output: webpackConfig.server.output(),
-  target: 'node',
-  externals: new RegExp(`^${Object.keys(pkg.dependencies).filter(d => d !== 'svelte').join('|')}`),
-  performance: {
-    hints: false, // it doesn't matter if server.js is large
+    target: 'web',
+    plugins: [
+      IS_DEV && new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      }),
+    ].filter(Boolean),
+    devtool: IS_DEV && 'inline-source-map',
   },
-}]);
+]);
+
+const server = merge([
+  serverConfig,
+  {
+    entry: webpackConfig.server.entry(),
+    output: webpackConfig.server.output(),
+    target: 'node',
+    externals: new RegExp(
+      `^${Object.keys(pkg.dependencies)
+        .filter(d => d !== 'svelte')
+        .join('|')}`,
+    ),
+    performance: {
+      hints: false, // it doesn't matter if server.js is large
+    },
+  },
+]);
 
 const serviceworker = {
   entry: webpackConfig.serviceworker.entry(),
@@ -40,4 +49,4 @@ const serviceworker = {
   mode: process.env.NODE_ENV,
 };
 
-module.exports = { client, server, serviceworker }
+module.exports = { client, server, serviceworker };

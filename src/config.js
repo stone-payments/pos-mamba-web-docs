926c73bd829e6353e40a65d3fbb9040ac5b0f130
Jaime C Marques <jaime.mqu@gmail.com>
2018-12-04 18:12:55 -0200

    Remove unnecessary props

--- src/config.js
+++ src/config.js
@@ -2,25 +2,25 @@ const path = require('path');
 
 module.exports = {
   server: {
-    ports: {
+    port: {
       dev: 3000,
       prod: 80,
-      secure: 443
     },
     credentials: {
-      staging: {
-        key: path.join(process.cwd(), "key-staging.pem"),
-        cert: path.join(process.cwd(), "cert-staging.pem"),
-        password: "1234"
+      dev: {
+        key: path.join(process.cwd(), 'localhost.key'),
+        cert: path.join(process.cwd(), 'localhost.cert'),
+        requestCert: false,
+        rejectUnauthorized: false,
       },
       prod: {
-        key: "/home/deployusr/cert/_.stone.com.br.key",
-        cert: "/home/deployusr/cert/_.stone.com.br.crt", 
+        key: '/home/deployusr/cert/_.stone.com.br.key',
+        cert: '/home/deployusr/cert/_.stone.com.br.crt',
         ca: [
-          "/home/deployusr/cert/gd_bundle-g2-g1.crt",
-          "/home/deployusr/cert/be94a68b8af575bd.crt"
-        ]
-      }
-    }
-  }
-}
+          '/home/deployusr/cert/gd_bundle-g2-g1.crt',
+          '/home/deployusr/cert/be94a68b8af575bd.crt',
+        ],
+      },
+    },
+  },
+};

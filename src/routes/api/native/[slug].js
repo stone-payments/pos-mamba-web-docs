619c750ede2467f1b8bb257866289148ee53088a
Jaime C Marques <jaime.mqu@gmail.com>
2018-12-04 18:30:58 -0200

    Format file

--- src/routes/api/native/[slug].js
+++ src/routes/api/native/[slug].js
@@ -11,14 +11,15 @@ export async function get(req, res, next) {
   const { slug } = req.params;
 
   if (!lookup || !lookup.has(slug)) {
-
     lookup = new Map();
 
     const packageRoot = path.join(process.cwd(), 'packages/pos/docs');
-    
-    const hasProperSlug = fs.readdirSync(packageRoot).filter(name => name.toLowerCase() === `${slug}.md`);
 
-    if(!hasProperSlug.length) {
+    const hasProperSlug = fs
+      .readdirSync(packageRoot)
+      .filter(name => name.toLowerCase() === `${slug}.md`);
+
+    if (!hasProperSlug.length) {
       console.warn(strings.errors.notFound);
       res.writeHead(404, {
         'Content-Type': 'application/json',
@@ -29,7 +30,7 @@ export async function get(req, res, next) {
     }
 
     const Slug = hasProperSlug[0];
-    
+
     // Get directory README.md and CHANGELOG.md sections
     const sections = getSections(path.join(packageRoot, Slug), {
       anchorPath: `native/${slug}`,
@@ -37,21 +38,29 @@ export async function get(req, res, next) {
     });
 
     const section = sections.find(item => item.slug === slug);
-    
-    const components = { docs: { html: section && section.html || '', metadata: { title: slug.charAt(0).toUpperCase() + slug.slice(1) }}, scope: constants.npmScope };
 
-    components.info = getPkgInfo(path.join(packageRoot, '../package.json'), section.file);
+    const components = {
+      docs: {
+        html: (section && section.html) || '',
+        metadata: { title: slug.charAt(0).toUpperCase() + slug.slice(1) },
+      },
+      scope: constants.npmScope,
+    };
+
+    components.info = getPkgInfo(
+      path.join(packageRoot, '../package.json'),
+      section.file,
+    );
 
     lookup.set(slug, JSON.stringify(components));
   }
 
   if (lookup.has(slug)) {
-    
     res.set({
       'Content-Type': 'application/json',
       'Cache-Control': `max-age=${5 * 60 * 1e3}`, // 5 minutes
     });
 
-    res.end(lookup.get(slug))
+    res.end(lookup.get(slug));
   }
 }

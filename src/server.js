f2b64eefd39bc9e7f770e4bf36dd1c8bd35fac49
Jaime C Marques <jaime.mqu@gmail.com>
2018-12-04 18:30:46 -0200

    Crean and refactor server.js

--- src/server.js
+++ src/server.js
@@ -1,24 +1,38 @@
-import dotenv from "dotenv";
-import express from "express";
-import serve from "serve-static";
-import compression from "compression";
-import { Store } from "svelte/store.js";
+import dotenv from 'dotenv';
+import express from 'express';
+import serve from 'serve-static';
+import compression from 'compression';
+import { Store } from 'svelte/store.js';
 import * as sapper from '../__sapper__/server.js';
-import http from "http";
-import https from "https";
-import fs from "fs";
-import path from "path";
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
+import serverConfig from './config.js';
+import createCredentials from './createCredentials.js';
+
+const IS_DEV = process.env.NODE_ENV === 'development';
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+const {
+  server: { credentials, port },
+} = serverConfig;
 
 dotenv.config();
 
 const app = express();
 
-const serveMisc = ["Icon", "Range/example", "Sprite/example", "QRCode/example"]
-  .map(path => serve(`packages/components/${path}`, { dotfiles: 'ignore', etag: false }));
+const serveMisc = [
+  'Icon',
+  'Range/example',
+  'Sprite/example',
+  'QRCode/example',
+].map(path =>
+  serve(`packages/components/${path}`, { dotfiles: 'ignore', etag: false }),
+);
 
 app.use(
   compression({ threshold: 0 }),
-  serve("static"),
+  serve('static'),
   serveMisc,
   sapper.middleware({
     store: req => {
@@ -26,34 +40,14 @@ app.use(
         guide_contents: [],
       });
     },
-  })
+  }),
 );
 
-if (process.env.NODE_ENV === 'development') {
-  app.listen(3000);
-} else {
-  const privateKey = fs.readFileSync("/home/deployusr/cert/_.stone.com.br.key");
+const secureConfig = IS_DEV ? credentials.dev : credentials.prod;
+const envPort = process.env.PORT || 3000;
 
-  const certificate = fs.readFileSync(
-    "/home/deployusr/cert/_.stone.com.br.crt"
-  );
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(createCredentials(secureConfig), app);
 
-  const ca = [
-    fs.readFileSync("/home/deployusr/cert/gd_bundle-g2-g1.crt"),
-    fs.readFileSync("/home/deployusr/cert/be94a68b8af575bd.crt"),
-  ];
-
-  const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca,
-  };
-
-  const httpServer = http.createServer(app);
-
-  const httpsServer = https.createServer(credentials, app);
-
-  httpServer.listen(80);
-
-  httpsServer.listen(443);
-}
+httpServer.listen(envPort);
+httpsServer.listen(443);

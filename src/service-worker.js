818cf80f8cf2ff780f20165e9bf2469f684ff67a
Jaime C Marques <jaime.mqu@gmail.com>
2019-02-22 17:35:29 -0300

    internal: 🏭 Update service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,9 @@
-import { timestamp, files, shell, routes } from '../__sapper__/service-worker.js';
+import {
+  files,
+  shell,
+  routes,
+  timestamp,
+} from '../__sapper__/service-worker.js';
 import NProgress from 'nprogress';
 
 const ASSETS = `cache${timestamp}`;
@@ -15,7 +20,7 @@ self.addEventListener('install', event => {
       .then(cache => cache.addAll(to_cache))
       .then(() => {
         self.skipWaiting();
-      })
+      }),
   );
 });
 
@@ -28,12 +33,13 @@ self.addEventListener('activate', event => {
       }
 
       self.clients.claim();
-    })
+    }),
   );
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET' || event.request.headers.has('range')) return;
+  // if (event.request.method !== 'GET' || event.request.headers.has('range')) return;
+  if (event.request.method !== 'GET') return;
 
   const url = new URL(event.request.url);
 
@@ -41,7 +47,11 @@ self.addEventListener('fetch', event => {
   if (!url.protocol.startsWith('http')) return;
 
   // ignore dev server requests
-  if (url.hostname === self.location.hostname && url.port !== self.location.port) return;
+  if (
+    url.hostname === self.location.hostname &&
+    url.port !== self.location.port
+  )
+    return;
 
   // always serve assets and webpack-generated files from cache
   if (url.host === self.location.host && cached.has(url.pathname)) {
@@ -59,25 +69,29 @@ self.addEventListener('fetch', event => {
   }
   */
 
-  if (event.request.cache === 'only-if-cached') return;
+  // if (event.request.cache === 'only-if-cached') return;
 
   // for everything else, try the network first, falling back to
   // cache if the user is offline. (If the pages never change, you
   // might prefer a cache-first approach to a network-first one.)
   event.respondWith(
-    caches
-      .open(`offline${timestamp}`)
-      .then(async cache => {        
-        try {
-          const response = await fetch(event.request);
-          cache.put(event.request, response.clone());
-          return response;
-        } catch(err) {
-          NProgress.done();
-          const response = await cache.match(event.request);
-          if (response) return response;
-          throw err;
+    caches.open(`offline${timestamp}`).then(async cache => {
+      try {
+        if (event.request.cache === 'only-if-cache') {
+          // workaround Chrome devtools bug https://github.com/sveltejs/sapper-template/issues/34
+          event.request.mode = 'same-origin';
         }
-      })
+
+        const response = await fetch(event.request);
+        cache.put(event.request, response.clone());
+        return response;
+      } catch (err) {
+        NProgress.done();
+        const response = await cache.match(event.request);
+        if (response) return response;
+
+        throw err;
+      }
+    }),
   );
 });

0267b19938f3ff6c5093451c1ae0bf32bd1c1809
Dico <didiraja@hotmail.com>
2020-01-15 18:34:16 -0300

    feat: 🎸 Input, Icon, Dialog, Container, Collection included

--- embed/src/routes.js
+++ embed/src/routes.js
@@ -1,9 +1,19 @@
 import Home from './routes/Home.html';
 import Button from './routes/Button.html';
 import Carousel from './routes/Carousel.html';
+import Collection from './routes/Collection.html';
+import Container from './routes/Container.html';
+import Dialog from './routes/Dialog.html';
+import Icon from './routes/Icon.html';
+import Input from './routes/Input.html';
 
 export default {
   '/': Home,
   '/button': Button,
   '/carousel': Carousel,
+  '/collection': Collection,
+  '/container': Container,
+  '/dialog': Dialog,
+  '/icon': Icon,
+  '/input': Input,
 };

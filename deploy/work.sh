1fd64d7e2077896b2f3acc63f577d2ad0c7292c1
Jaime Costa Marques <jaime.mqu@gmail.com>
2021-01-25 16:15:48 -0300

    refactor: 💡 update work regex and run

--- deploy/work.sh
+++ deploy/work.sh
@@ -18,8 +18,6 @@ chown -R $USER ./mamba-sdk/
 npm run build
 
 # Replace sapper loopback address to fix direct call to site inner pages
-{ rm __sapper__/build/server/server.js && awk '{gsub(/http\:\/\/127\.0\.0\.1/,"https://mambasdk-docs.stone.com.br/", $0); print}' > __sapper__/build/server/server.js; } < __sapper__/build/server/server.js
-
-echo pwd
+{ rm __sapper__/build/server/server.js && awk '{gsub(/http:\/\/127\.0\.0\.1/,"https://mambasdk-docs.stone.com.br/", $0); print}' > __sapper__/build/server/server.js; } < __sapper__/build/server/server.js
 
 pm2 restart ecosystem.config.js

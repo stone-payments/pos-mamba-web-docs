521023a8648abdb1e410712b8e584c0286a6a5c3
Jaime Costa Marques <jaime.mqu@gmail.com>
2021-01-25 20:49:27 -0300

    fix: 🐛 submodule need but not exist collision

--- deploy/work.sh
+++ deploy/work.sh
@@ -9,10 +9,12 @@ echo pwd
 # Update lasted update
 git pull
 
-# Install dependencies and submodule sdk
-npm i
+npm run pull:sdk:submodule
 
 sudo chown -R $USER ./mamba-sdk/
+
+# Install dependencies and submodule sdk
+npm i
 
 # Build project
 npm run build
@@ -20,4 +22,4 @@ npm run build
 # Replace sapper loopback address to fix direct call to site inner pages
 { rm __sapper__/build/server/server.js && awk '{gsub(/http:\/\/127\.0\.0\.1/,"https://mambasdk-docs.stone.com.br/", $0); print}' > __sapper__/build/server/server.js; } < __sapper__/build/server/server.js
 
-sudo pm2 restart ecosystem.config.js
+pm2 restart ecosystem.config.js
